e3b5e2f8ec926c7a303519519818759c00a6e346
azamiya <t.adaniya@adawarp.com>
2020-06-15 17:52:28 +0900

    Initial commit

---
+++ app/src/main/java/com/adawarp/dotinstall_tutorial/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.adawarp.dotinstall_tutorial
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun changeTextView(view: View) {
+        messageTextView.text = "Hello there!aaabbb"
+    }
+}
